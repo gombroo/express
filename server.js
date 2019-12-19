eee0ee73e137705897fd484ae3ef467a923be075
Ola Siem <info@siemieniuk.net>
2019-12-19 16:44:23 +0100

    Add 'use' method to 'res' object

--- server.js
+++ server.js
@@ -3,45 +3,31 @@ const path = require('path');
 
 const app = express();
 
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname + '/views/index.html'));
-// });
-
-// app.get('/about', (req, res) => {
-//   res.sendFile(path.join(__dirname + '/views/about.html'));
-// });
-
-// app.get('/contact', (req, res) => {
-//   res.sendFile(path.join(__dirname + '/views/contact.html'));
-// });
-
-// app.get('/info', (req, res) => {
-//   res.sendFile(path.join(__dirname + '/views/info.html'));
-// });
-
-// app.get('/history', (req, res) => {
-//   res.sendFile(path.join(__dirname + '/views/history.html'));
-// });
-
-app.use('/admin', (req, res, next) => {
-  if(isAdmin()) next();
-  else res.send('Go away!');
+app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname + `/views/${name}`));
+  };
+  next();
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/views/home.html'));
+  res.show('index.html');
 });
 
-app.get('/cart', (req, res) => {
-  res.sendFile(path.join(__dirname + '/views/cart.html'));
+app.get('/about', (req, res) => {
+  res.show('about.html');
 });
 
-app.get('/admin/products', (req, res) => {
-  res.sendFile(path.join(__dirname + '/views/admin/products.html'));
+app.get('/contact', (req, res) => {
+  res.show('contact.html');
 });
 
-app.get('/admin/payments', (req, res) => {
-  res.sendFile(path.join(__dirname + '/views/admin/payments.html'));
+app.get('/info', (req, res) => {
+  res.show('info.html');
+});
+
+app.get('/history', (req, res) => {
+  res.show('history.html');
 });
 
 app.listen(8000, () => {
