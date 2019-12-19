76cf4240cc0b8b66e20d4239bb676cc9a8a311e7
Ola Siem <info@siemieniuk.net>
2019-12-19 16:28:01 +0100

    Add middleware

--- server.js
+++ server.js
@@ -3,32 +3,45 @@ const path = require('path');
 
 const app = express();
 
-// app.get('/about', (req, res) => {
-//   res.send('<h1>About</h1>');
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/views/index.html'));
 // });
 
-// app.get('/', (req, res) => {
-//   res.sendFile('./views/index.html'); // not working, cant find root
+// app.get('/about', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/views/about.html'));
 // });
+
+// app.get('/contact', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/views/contact.html'));
+// });
+
+// app.get('/info', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/views/info.html'));
+// });
+
+// app.get('/history', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/views/history.html'));
+// });
+
+app.use('/admin', (req, res, next) => {
+  if(isAdmin()) next();
+  else res.send('Go away!');
+});
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/views/index.html'));
+  res.sendFile(path.join(__dirname + '/views/home.html'));
 });
 
-app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname + '/views/about.html'));
+app.get('/cart', (req, res) => {
+  res.sendFile(path.join(__dirname + '/views/cart.html'));
 });
 
-app.get('/contact', (req, res) => {
-  res.sendFile(path.join(__dirname + '/views/contact.html'));
+app.get('/admin/products', (req, res) => {
+  res.sendFile(path.join(__dirname + '/views/admin/products.html'));
 });
 
-app.get('/info', (req, res) => {
-  res.sendFile(path.join(__dirname + '/views/info.html'));
-});
-
-app.get('/history', (req, res) => {
-  res.sendFile(path.join(__dirname + '/views/history.html'));
+app.get('/admin/payments', (req, res) => {
+  res.sendFile(path.join(__dirname + '/views/admin/payments.html'));
 });
 
 app.listen(8000, () => {
